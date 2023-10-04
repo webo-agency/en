f71b1773e2eee2a537ddaf7dfd0185ec041d8550
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-10-04 18:11:31 +0200

    Add Nuxt.studio

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  name: "webo-en",
   extends: ["@webo-agency/theme"],
+  target: "static",
+  modules: ["@nuxthq/studio"],
 });
