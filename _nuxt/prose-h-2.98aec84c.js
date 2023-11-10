750d3360be1f10008353203cccb6f9b8c32b9b62
kamilmedrala <81763542+kamilmedrala@users.noreply.github.com>
2023-11-10 15:17:33 +0000

    Deploying to gh-pages from @ webo-agency/en@0937e26d3384defad28e5486dbf2cfdc94c064cb 🚀

--- _nuxt/prose-h-2.98aec84c.js
+++
@@ -1,1 +0,0 @@
-import{k as r,b as o,c as s,D as t}from"./entry.a4899efe.js";const c={};function n(e,a){return o(),s("h2",null,[t(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as default};
