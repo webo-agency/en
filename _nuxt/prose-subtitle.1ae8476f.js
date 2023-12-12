57a000bf76f6d92e4f9f043e146cd9072e78878d
TK-54-4B <99447051+TK-54-4B@users.noreply.github.com>
2023-12-12 15:51:25 +0000

    Deploying to gh-pages from @ webo-agency/en@34657b425203136b877e9392372066d725736511 🚀

---
+++ _nuxt/prose-subtitle.1ae8476f.js
@@ -0,0 +1,1 @@
+import{r as t}from"./slot.c546e470.js";import{k as s,b as o,c as r,e as a}from"./entry.bae8d384.js";import"./node.e4a9c3bc.js";const c={},n={class:"subtitle"};function l(e,i){return o(),r("div",null,[a("span",n,[t(e.$slots,"default")])])}const p=s(c,[["render",l]]);export{p as default};
