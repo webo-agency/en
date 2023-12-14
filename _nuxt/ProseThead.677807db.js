79818e655344f62dc300a142e84a8a222ac2306c
TK-54-4B <99447051+TK-54-4B@users.noreply.github.com>
2023-12-14 12:39:35 +0000

    Deploying to gh-pages from @ webo-agency/en@9f933275628cf94b3a71ea78f4aac53094c48ff5 🚀

---
+++ _nuxt/ProseThead.677807db.js
@@ -0,0 +1,1 @@
+import{k as r,b as t,c as o,E as s}from"./entry.7fadf4f2.js";const a={};function c(e,n){return t(),o("thead",null,[s(e.$slots,"default")])}const d=r(a,[["render",c]]);export{d as default};
