15c098e84aebba82f3ec91d7273b238575fe537f
kamilmedrala <81763542+kamilmedrala@users.noreply.github.com>
2024-01-11 16:53:42 +0000

    Deploying to gh-pages from @ webo-agency/en@c67c6e4f8230b152e39840912c54bf4749ba89df 🚀

---
+++ _nuxt/Markdown.bKxrHNXf.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.oWD42GRG.js";import{d as o,aq as u,J as f,a4 as m}from"./entry.GFRmWXYq.js";import"./node.uMQX-T_c.js";const d=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{d as default};
