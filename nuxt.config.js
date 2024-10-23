95a7b434cf117ec18bd7402b8525d311a2a128b2
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-10-23 20:16:16 +0200

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   name: "webo-en",
-  extends: ["@webo-agency/theme"],
+  extends: ["github:webo-agency/www#main"],
   target: "static",
   modules: ["@nuxthq/studio"],
   site:{
