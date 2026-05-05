96897e4e1bce518eae4efc855e93dac5aa37a6df
Kamil Mędrala <k.medrala99@gmail.com>
2026-05-05 11:44:03 +0200

    fix: [WEBOWEB-277] versions fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   name: "webo-en",
-  extends: ["github:webo-agency/www#main"],
+  extends: ["@webo-agency/theme"],
   target: "static",
   modules: ["@nuxthq/studio"],
   site:{
